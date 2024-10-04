e136fbc0ff40202d8a12a4ddfe46fe2ecd7c304a
EdAlekseiev <e.alekseiev@gmail.com>
2024-10-04 14:50:43 +0300

    Solution

---
+++ src/app/DataHandler.java
@@ -0,0 +1,21 @@
+package app;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class DataHandler {
+
+    String[] fruits = new DataRepository().getData();
+
+    public void getOutput() {
+        synchronized(this) {
+            StringBuilder sb = new StringBuilder();
+            AtomicInteger count = new AtomicInteger(1);
+            for (String fruit : fruits) {
+                sb.append(String.format("(%d) %s ", count.get(), fruit));
+                count.getAndIncrement();
+            }
+            Thread currentThread = Thread.currentThread();
+            System.out.println(currentThread.getName() + ": " + sb);
+        }
+    }
+}
